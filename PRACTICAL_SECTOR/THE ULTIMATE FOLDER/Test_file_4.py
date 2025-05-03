c3dc23634df46582f7039f5bdf35fc90a73d604a
M0bile132024 <22chimaobisoronnadi@st-ambrosecollege.org.uk>
2025-05-03 21:01:25 +0000

    Update THE ULTIMATE CALC.py to ver 2; others category revamp:add new chemistry/physics/ratio calculations  and clipboard fixing, and modify .gitignore to ingore ********

--- PRACTICAL_SECTOR/THE ULTIMATE FOLDER/Test_file_4.py
+++ PRACTICAL_SECTOR/THE ULTIMATE FOLDER/Test_file_4.py
@@ -1,25 +1,11 @@
-print("You have selected find atomic weight using relative mass calc!")
-mass_list = []
-abundance_list = []
-product_list = []
-while True:
-  
-  
-    mass = input("Enter the mass of a known isotope (or type 'done' to finish): ")
-    if mass == 'done':
-        break
-    else:
-        abundance = input("Enter the abundance of the known isotope: ")
-        mass_list.append(float(mass))
-        abundance_list.append(float(abundance))
-        product = float(mass) * float(abundance)
-        product_list.append(product)
-relative_mass = float(input("Enter the relative mass of the isotope: "))
-abunance = 100 - sum(abundance_list)
-atomic_weight = relative_mass * 100
-for i in range(len(mass_list)):
-    atomic_weight -= product_list[i]
-atomic_weight /= abunance
-print("The atomic weight of the unknown isotope is: ", atomic_weight)
+import os
+clippy = os.environ.get('CLIPBOARD')  # This will return None if the environment variable is not set
+if clippy is not None:
+    print("CLIPBOARD environment variable is set to:", clippy)
+else:
+    print("CLIPBOARD environment variable is not set.")    
+
+
+
 
 
